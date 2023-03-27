3331b09bb452b4f34c94eec2695c310a6f480eeb
DXF0530 <dmitry.fleish@sbdinc.com>
2023-03-27 22:07:19 +0300

    Initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-dev-bytes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
