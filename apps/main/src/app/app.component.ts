7ec99703587855dadb697b6a8846857bf9cb81a3
DXF0530 <dmitry.fleish@sbdinc.com>
2023-03-28 21:39:06 +0300

    init

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'angular-dev-bytes-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    App
+  `,
+  styles: [],
+  standalone: true
 })
 export class AppComponent {
   title = 'main';
