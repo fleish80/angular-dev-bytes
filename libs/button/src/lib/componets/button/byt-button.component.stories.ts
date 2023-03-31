416f604c97f275bb896dff595f931347bcf5bc9c
DXF0530 <dmitry.fleish@sbdinc.com>
2023-03-31 09:02:08 +0300

    finish with story book

--- libs/button/src/lib/componets/button/byt-button.component.stories.ts
+++ libs/button/src/lib/componets/button/byt-button.component.stories.ts
@@ -1,14 +1,38 @@
-import { Meta } from '@storybook/angular';
-import { BytButtonComponent } from './byt-button.component';
+import {Meta, Story} from '@storybook/angular';
+import {BytButtonComponent} from './byt-button.component';
 
 export default {
   title: 'BytButtonComponent',
   component: BytButtonComponent,
 } as Meta<BytButtonComponent>;
 
-export const Primary = {
-  render: (args: BytButtonComponent) => ({
-    props: args,
-  }),
-  args: {},
-};
+const Template: Story<BytButtonComponent> = (args: BytButtonComponent) => ({
+  moduleMetadata: {
+    imports: [BytButtonComponent]
+  },
+  template: `<button byt-button [color]="color">Primary</button>`,
+  props: {
+    ...args
+  }
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  color: 'primary'
+}
+
+export const NegativePrimary = Template.bind({});
+NegativePrimary.args = {
+  color: 'negative'
+}
+
+// export const Primary = () => ({
+//   // render: (args: BytButtonComponent) => ({
+//   //   props: args,
+//   // }),
+//   // args: {},
+//   moduleMetadata: {
+//     imports: [BytButtonComponent]
+//   },
+//   template: `<button byt-button>Primary</button>`
+// });
--- libs/button/src/lib/componets/button/byt-button.component.ts
+++ libs/button/src/lib/componets/button/byt-button.component.ts
@@ -1,12 +1,64 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {FocusMonitor} from '@angular/cdk/a11y';
 
 @Component({
-  selector: 'byt-button',
+  selector: '[byt-button]',
   standalone: true,
   imports: [],
-  template: ``,
-  styles: [],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  template: `
+    <ng-content/>`,
+  styles: [`
+    :host {
+      height: 2rem;
+      padding: 0 0.75rem;
+      box-sizing: border-box;
+      border-radius: 8px;
+    }
+
+    :host(.byt-primary) {
+      background-color: #e6f4ff;
+      border: 1px solid #bfe3ff;
+      color: #008dff;
+    }
+
+    :host(.byt-negative) {
+      background-color: #fce8ee;
+      border: 1px solid #f7c6d4;
+      color: #de1a51;
+    }
+    :host(:focus) {
+      outline: none;
+    }
+    :host(.byt-primary.cdk-keyboard-focused) {
+      box-shadow: 0 0 0 3px rgba(0, 141, 255, 20%);
+    }
+    :host(.byt-negative.cdk-keyboard-focused) {
+      box-shadow: 0 0 0 3px rgba(222, 26, 81, 20%);
+    }
+
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.byt-primary]': 'color === "primary"',
+    '[class.byt-negative]': 'color === "negative"'
+  }
 })
-export class BytButtonComponent {
+export class BytButtonComponent implements OnInit, OnDestroy {
+
+
+  /**
+   * Define the appearance of the button
+   */
+  @Input() color: 'primary' | 'negative' = 'primary';
+
+  #elementRef = inject(ElementRef<HTMLButtonElement>);
+  #focusMonitor = inject(FocusMonitor);
+
+  ngOnInit(): void {
+    this.#focusMonitor.monitor(this.#elementRef);
+  }
+
+  ngOnDestroy(): void {
+    this.#focusMonitor.stopMonitoring(this.#elementRef);
+  }
 }
